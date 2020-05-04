050d5854ba9d668648f4fc2bf500cea3e6200560
Brian Monaccio <brianmonaccio@gmail.com>
2020-05-04 12:38:58 -0400

    Completes removal of react-bootstrap from the project

--- src/App/Layout/Layout.tsx
+++ src/App/Layout/Layout.tsx
@@ -20,12 +20,6 @@ const Layout: React.FC<Props> = ({ children }) => {
     <>
       <Helmet>
         <link rel="stylesheet" href="https://use.typekit.net/ari5qhg.css" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-          crossOrigin="anonymous"
-        />
       </Helmet>
       <Container component={'main'} maxWidth={'lg'}>
         {children}
