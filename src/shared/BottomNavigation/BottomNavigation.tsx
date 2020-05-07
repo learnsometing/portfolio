4daa48f564831f91ae293dc2b58437c5f20b50de
Brian Monaccio <brianmonaccio@gmail.com>
2020-05-07 11:35:09 -0400

    Changes Toggle button to a BottomNavigation bar that has a menu button on it.

--- src/shared/BottomNavigation/BottomNavigation.tsx
+++ src/shared/BottomNavigation/BottomNavigation.tsx
@@ -3,48 +3,64 @@ import styled from 'styled-components';
 import Hamburger from 'hamburger-react';
 
 import Paper from '@material-ui/core/Paper';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Typography from '@material-ui/core/Typography';
 
 import NavigationDrawer from './NavigationDrawer';
 
-const Toggle = styled(Paper)`
-  width: 60px;
-  height: 60px;
+const Navbar = styled(Paper).attrs({
+  elevation: 2,
+  square: true,
+})`
   position: fixed;
   bottom: 0;
-  right: 0;
+  height: 80px;
+  width: 100%;
   z-index: 2;
-  margin-bottom: 1rem;
-  margin-right: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
   background-color: #fdfde8;
-
   @media only screen and (min-width: 768px) {
     display: none;
   }
 `;
 
-const CollapsedNavigation: React.FC = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+const ActionLabel = styled(Typography).attrs({
+  variant: 'h6',
+  component: 'span',
+})`
+  color: #f95738;
+`;
+
+const BottomNavigationBar: React.FC = () => {
+  const [value, setValue] = useState(0);
+  const [isNavDrawerOpen, setIsNavDrawerOpen] = useState(false);
 
   const toggleDrawer = (): void => {
-    setIsDrawerOpen(!isDrawerOpen);
+    setIsNavDrawerOpen(!isNavDrawerOpen);
   };
 
   return (
     <>
-      <Toggle elevation={2}>
-        <Hamburger
-          color="#f95738"
-          toggled={isDrawerOpen}
-          toggle={toggleDrawer}
+      <BottomNavigation
+        component={Navbar}
+        value={value}
+        onChange={(event, newValue): void => setValue(newValue)}
+        showLabels
+      >
+        <BottomNavigationAction
+          label={<ActionLabel>Menu</ActionLabel>}
+          icon={
+            <Hamburger
+              color="#f95738"
+              toggled={isNavDrawerOpen}
+              toggle={toggleDrawer}
+            />
+          }
         />
-      </Toggle>
-      <NavigationDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+      </BottomNavigation>
+      <NavigationDrawer isOpen={isNavDrawerOpen} />
     </>
   );
 };
 
-export default CollapsedNavigation;
+export default BottomNavigationBar;
--- src/shared/BottomNavigation/NavigationDrawer.tsx
+++ src/shared/BottomNavigation/NavigationDrawer.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Hamburger from 'hamburger-react';
+// import Hamburger from 'hamburger-react';
 
-import Paper from '@material-ui/core/Paper';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-
 import { NavLink } from '../Navigation';
 
+const MenuDrawer = styled(Drawer)`
+  height: calc(100vh - 80px);
+`;
+
 const Menu = styled(List)`
-  height: 100vh;
+  height: calc(100vh - 80px);
   width: 100vw;
   background-color: #fdfde8;
 `;
@@ -34,37 +36,18 @@ const MenuLink = styled(NavLink)`
   align-items: center;
 `;
 
-const Toggle = styled(Paper)`
-  width: 60px;
-  height: 60px;
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  z-index: 2;
-  margin-bottom: 1rem;
-  margin-right: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  background-color: #fdfde8;
-
-  @media only screen and (min-width: 768px) {
-    display: none;
-  }
-`;
-
 interface NavigationDrawerProps {
   isOpen: boolean;
-  toggleDrawer: () => void;
 }
 
-const NavigationDrawer: React.FC<NavigationDrawerProps> = ({
-  isOpen,
-  toggleDrawer,
-}) => {
+const NavigationDrawer: React.FC<NavigationDrawerProps> = ({ isOpen }) => {
   return (
-    <Drawer anchor={'top'} open={isOpen}>
+    <MenuDrawer
+      anchor={'top'}
+      open={isOpen}
+      ModalProps={{ hideBackdrop: true, disableBackdropClick: true }}
+      elevation={2}
+    >
       <Menu>
         <MenuItem>
           <MenuLink to="/">
@@ -90,7 +73,6 @@ const NavigationDrawer: React.FC<NavigationDrawerProps> = ({
           </MenuLink>
         </MenuItem>
         <Divider component={'li'} />
-
         <MenuItem>
           <MenuLink to="/contact">
             <Typography variant={'h2'} component="span">
@@ -98,18 +80,13 @@ const NavigationDrawer: React.FC<NavigationDrawerProps> = ({
             </Typography>
           </MenuLink>
         </MenuItem>
-
-        <Toggle elevation={2}>
-          <Hamburger color="#f95738" toggled={isOpen} toggle={toggleDrawer} />
-        </Toggle>
       </Menu>
-    </Drawer>
+    </MenuDrawer>
   );
 };
 
 NavigationDrawer.propTypes = {
   isOpen: PropTypes.bool.isRequired,
-  toggleDrawer: PropTypes.func.isRequired,
 };
 
 export default NavigationDrawer;
